65b358b249b415709ec314646514e25999a5b296
maxmayermader <maxmayermader2@gmail.com>
2024-11-12 23:06:00 -0600

    submittted

--- HomeWork5/NewHW5/main.py
+++ HomeWork5/NewHW5/main.py
@@ -7,7 +7,7 @@ def main():
     # Define the state bounds [x, y, θ]
     stateBounds = [(-3, 3), (-1, 1), (-np.pi, np.pi)]
 
-    # Define circular obstacles 
+    # Define circular obstacles
     centers = [(0, -1), (0, 1)]  # Centers of half circles
     radii = [0.8, 0.8]  # radius = 1 - dt, where dt = 0.2
 
@@ -31,13 +31,11 @@ def main():
     path, success = planner.plan(startState, goalState)
     print(f"Success: {path}")
 
-    # Visualizer
+    # Create Plot
     drawer = CSpaceDrawer(stateBounds[:2])
 
     # Draw obstacles
     drawer.drawObstacles(centers, radii)
-
-
 
     if success:
         drawer.drawGraph(planner.graph.vertices, planner.graph.edges)
@@ -50,10 +48,10 @@ def main():
         drawer.drawGraph(planner.graph.vertices, planner.graph.edges)
         print("No path found!")
 
-    # Draw start and goal configurations with orientation arrows
+    # Draw start and goal configurations with orientation 
     drawer.drawPoint(startState, color='green', size=200)
     drawer.drawPoint(goalState, color='red', size=200)
-    # Add title and show plot
+
     drawer.ax.set_title("RRT Path Planning for Dubins Car", fontsize=16)
     drawer.show()
 
