6a0b77044765065a6b60918299e2a92c256b5d5f
= <manas1@iastate.edu>
2024-11-25 17:14:21 -0600

    Cleaned it up

--- FinalProject/plot.py
+++ FinalProject/plot.py
@@ -1,5 +1,3 @@
-from mpl_toolkits import mplot3d
-from mpl_toolkits.mplot3d import Axes3D 
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -22,35 +20,6 @@ def plot(xlim, ylim, zlim, obstacles):
 
     plt.show()
 
-# def createObs(x0,y0,z0,r0):
-#     xlim = np.array((-3,3))
-#     ylim = np.array((-3,3))
-#     zlim = np.array((-3,3))
-
-#     theta = np.linspace(0, 2 * np.pi, 100)
-#     phi = np.linspace(0, np.pi, 50)
-#     theta, phi = np.meshgrid(theta, phi)
-#     r = 1 * 0.7
-#     x = r * np.sin(phi) * np.cos(theta) 
-#     y = r * np.sin(phi) * np.sin(theta) 
-#     z = r * np.cos(phi)
-#     print(f"Shape of x, y, z: {x.shape}")
-#     print(f"Min and max values - x: ({x.min():.2f}, {x.max():.2f}), y: ({y.min():.2f}, {y.max():.2f}), z: ({z.min():.2f}, {z.max():.2f})")
-
-#     ax = plt.axes(projection='3d')
-#     plt.xlim(xlim)
-#     plt.ylim(ylim)
-#     ax.set_zlim(zlim)
-    
-#     # fig = plt.figure(figsize=(10, 10))
-#     # ax = fig.add_subplot(111, projection='3d')
-#     ax.plot_surface(x, y, z, cmap='magma', alpha=0.8)
-#     ax.set_xlabel('X')
-#     ax.set_ylabel('Y')
-#     ax.set_zlabel('Z')
-#     ax.set_title('3D Sphere')
-#     ax.set_box_aspect((1, 1, 1))
-#     plt.show()
 
 
 
